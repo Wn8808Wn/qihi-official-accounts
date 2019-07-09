41f33d11fd3f16dbd08ea880b0d6e6b24b570489
wangning <18511848808@163.com>
2019-07-09 14:50:30 +0800

    2019.7.9-1版本1

--- qihi-official-accounts/config/index.js
+++ qihi-official-accounts/config/index.js
@@ -11,8 +11,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target:'http://192.168.1.145:8080',
-        // target:'http://39.98.82.63:8080',
+        // target:'http://192.168.1.145:8080',
+        target:'http://39.98.82.63:8080',
         changeOrigin:true,
         pathRewrite:{
           '^/api':'/'
@@ -21,8 +21,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '192.168.1.119', // can be overwritten by process.env.HOST
-    // host: '192.168.1.105', // can be overwritten by process.env.HOST
+    // host: '192.168.1.119', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
